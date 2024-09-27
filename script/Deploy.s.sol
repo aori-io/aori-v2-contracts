eca44073337152742bfc6240c244db9f7ccd1039
Hilliam T <hilliamtung@gmail.com>
2024-09-27 12:00:42 +0100

    deploy script update version to v2.3

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.13;
 
 import "forge-std/Script.sol";
 import {ICREATE3Factory} from "create3-factory/src/ICREATE3Factory.sol";
-import { MultichainDeployScript } from "./MultichainDeploy.s.sol";
+import {MultichainDeployScript} from "./MultichainDeploy.s.sol";
 import "../src/AoriV2.sol";
 import "../src/AoriV2Blast.sol";
 
@@ -11,10 +11,16 @@ contract DeployScript is Script, MultichainDeployScript {
     function run() external {
         uint256 deployerPrivateKey = uint256(vm.envBytes32("PRIVATE_KEY"));
         address deployerAddress = vm.addr(deployerPrivateKey);
-        bytes memory bytecode = abi.encodePacked(type(AoriV2).creationCode, abi.encode(deployerAddress));
-        bytes memory blastBytecode = abi.encodePacked(type(AoriV2Blast).creationCode, abi.encode(deployerAddress));
+        bytes memory bytecode = abi.encodePacked(
+            type(AoriV2).creationCode,
+            abi.encode(deployerAddress)
+        );
+        bytes memory blastBytecode = abi.encodePacked(
+            type(AoriV2Blast).creationCode,
+            abi.encode(deployerAddress)
+        );
 
-        string memory AORI_VERSION = "Aori v2.2";
+        string memory AORI_VERSION = "Aori v2.3";
 
         /*//////////////////////////////////////////////////////////////
                                     TESTNETS
